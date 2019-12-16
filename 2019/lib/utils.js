3c78142582b028c82827ad9904b61f633f0c1b70
Wesley Mao <wjmao88@gmail.com>
2019-12-16 16:23:21 -0500

    15

--- 2019/lib/utils.js
+++ 2019/lib/utils.js
@@ -1,8 +1,41 @@
+const { map } = require('lodash');
 
-module.exports.fillSparse = function fillSparse(list) {
+module.exports.fillSparse = fillSparse;
+function fillSparse(list) {
   return [...list.keys()].map(k => list[k]);
 }
 
-module.exports.flatten =function flatten(list) {
+module.exports.mapToArrays = mapToArrays;
+function mapToArrays(tileMap) {
+  const yOffset = -Math.min(...Object.keys(tileMap).map(n => +n));
+  const xOffset = -Math.min(
+    ...map(tileMap, (row) => {
+      return Math.min(...Object.keys(row).map(n => +n))
+    })
+  );
+
+  const tileArrays = [];
+  for (y in tileMap) {
+    for (x in tileMap[y]) {
+      tileArrays[+y + yOffset] = tileArrays[+y + yOffset] || [];
+      tileArrays[+y + yOffset][+x + xOffset] = tileMap[y][x];
+    }
+  }
+
+  return {
+    tileArrays: fillSparse(tileArrays).map((row) => fillSparse(row || [])),
+    xOffset,
+    yOffset,
+  };
+}
+
+module.exports.flatten = function flatten(list) {
   return list.reduce((s,n) => s.concat(n), []);
+}
+
+module.exports.printMap = function printMap(tileMap, printTile) {
+  const { tileArrays, xOffset, yOffset, } = mapToArrays(tileMap);
+  return tileArrays.map((row, y) => {
+    return row.map((t, x) => printTile(t, +x - xOffset, +y - yOffset)).join('');
+  }).join('\n');
 }
